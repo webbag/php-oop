19e0f9224a46d1908e4974865183a387cb52f9eb
kris <kris>
2018-07-18 23:12:22 +0200

    new features 5.5

---
+++ website/public/55.php
@@ -0,0 +1,18 @@
+<?php
+function xrange($start, $limit, $step = 1)
+{
+    for ($i = $start; $i <= $limit; $i += $step) {
+        yield $i;
+    }
+}
+
+echo 'Single digit odd numbers: ';
+
+/*
+ * Note that an array is never created or returned,
+ * which saves memory.
+ */
+foreach (xrange(1, 9, 2) as $number) {
+    echo "$number ";
+}
+
